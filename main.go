3eef545efeaa3c079498074b667f0520a02499ee
JDode <2420498526@qq.com>
2019-07-05 12:40:09 +0800

    vscode edit

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import "fmt"
 Create BY YooDing
 Des: gone application main function
 Time: 2019-07-05 11:07:32
-<a href="https://github.com/YooDing/gone">Github<a>
+<a href="https://github.com/YooDing/gone">Github<a>∏
 */
 func main() {
 	fmt.Print("Hello gone! 还没有开始动工写哟~~")
