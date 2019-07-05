d2d867f08dc71060bbdd6f46f9eb078db5b8e7d4
JDode <2420498526@qq.com>
2019-07-05 13:34:17 +0800

    add utils package🚀

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 )
 
 /**
-Create BY YooDing
-Des: gone application main function
-Time: 2019-07-05 11:07:32
-<a href="https://github.com/YooDing/gone">Github<a>∏
-*/
+ *
+ * Create BY YooDing
+ *
+ * Des: gone application main function
+ *
+ * Time: 2019年07月05日13:28:19
+ *
+ * <a href="https://github.com/YooDing/gone">Github<a>∏
+ */
 func main() {
 	fmt.Println("Hello gone! 还没有开始动工写哟~~")
-	utils.Done("2019年07月05日12:52:01")
+	utils.Done("YooDing")
 }
--- utils/console.go
+++ utils/console.go
@@ -2,8 +2,23 @@ package utils
 
 import (
 	"fmt"
+	"time"
+	"strconv"
 )
-
+/**
+ *
+ * Create BY YooDing
+ *
+ * Des: system util
+ *
+ * Time: 2019年07月05日13:32:44
+ *
+ * <a href="https://github.com/YooDing/gone">Github<a>∏
+ */
 func Done(logStr string) {
-	fmt.Println(logStr)
+	fmt.Printf("\033[42;34m 「done」 \033[0m \033[32m %s --> %s \033[0m",NowTimeStr(),logStr)
+}
+func NowTimeStr()(timeStr string){
+	now := time.Now()
+	return strconv.Itoa(now.Year())+"-"+now.Month().String()+"-"+strconv.Itoa(now.Day())+" "+strconv.Itoa(now.Hour())+":"+strconv.Itoa(now.Minute())+":"+strconv.Itoa(now.Second())
 }
