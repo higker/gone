93e824cbd1cce0caacb2af3e76f722abd81066ec
JDode <2420498526@qq.com>
2019-07-05 11:15:48 +0800

    initialize this project 👏

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+/**
+Create BY YooDing
+des: gone application main function
+time: 2019-07-05 11:07:32
+<a href="https://github.com/YooDing/gone">Github<a>
+*/
+func main() {
+	fmt.Print("Hello gone~")
+}
