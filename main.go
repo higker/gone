d7fd40532ed96d75f494ed4afd3535183c56ed94
JDode <2420498526@qq.com>
2019-07-05 13:47:04 +0800

    success console 🚀

--- main.go
+++ main.go
@@ -13,9 +13,11 @@ import (
  *
  * Time: 2019年07月05日13:28:19
  *
- * <a href="https://github.com/YooDing/gone">Github<a>∏
+ * <a href="https://github.com/YooDing/gone">Github<a>
  */
 func main() {
 	fmt.Println("Hello gone! 还没有开始动工写哟~~")
-	utils.Done("YooDing")
+	utils.Done("完毕")
+	utils.Info("信息")
+	utils.Error("错误")
 }
--- utils/console.go
+++ utils/console.go
@@ -5,6 +5,7 @@ import (
 	"time"
 	"strconv"
 )
+
 /**
  *
  * Create BY YooDing
@@ -13,12 +14,19 @@ import (
  *
  * Time: 2019年07月05日13:32:44
  *
- * <a href="https://github.com/YooDing/gone">Github<a>∏
+ * <a href="https://github.com/YooDing/gone">Github<a>
  */
 func Done(logStr string) {
-	fmt.Printf("\033[42;34m 「done」 \033[0m \033[32m %s --> %s \033[0m",NowTimeStr(),logStr)
+	fmt.Printf("\033[42;34m 「Done」 \033[0m \033[32m %s --> %s \033[0m \n", NowTimeStr(), logStr)
 }
-func NowTimeStr()(timeStr string){
+func Info(logStr string) {
+	fmt.Printf("\033[44;30m 「Info」 \033[0m \033[34m %s --> %s \033[0m \n", NowTimeStr(), logStr)
+}
+func Error(logStr string) {
+	fmt.Printf("\033[41;30m 「Fail」 \033[0m \033[31m %s --> %s \033[0m \n", NowTimeStr(), logStr)
+}
+
+func NowTimeStr() (timeStr string) {
 	now := time.Now()
-	return strconv.Itoa(now.Year())+"-"+now.Month().String()+"-"+strconv.Itoa(now.Day())+" "+strconv.Itoa(now.Hour())+":"+strconv.Itoa(now.Minute())+":"+strconv.Itoa(now.Second())
+	return strconv.Itoa(now.Year()) + "-" + now.Month().String() + "-" + strconv.Itoa(now.Day()) + " " + strconv.Itoa(now.Hour()) + ":" + strconv.Itoa(now.Minute()) + ":" + strconv.Itoa(now.Second())
 }
