3f32521c36a7f735155dff6ca7468e6927a47b23
JDode <2420498526@qq.com>
2019-07-06 12:52:26 +0800

    add add file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"gone/component"
 	"gone/utils"
 
+	"gone/model"
 )
 
 /**
@@ -22,5 +23,5 @@ func main() {
 	//utils.Done("完毕")
 	//utils.Info("信息")
 	utils.Error("错误")
-
+	utils.Info(model.Server)
 }
--- model/conf_data.go
+++ model/conf_data.go
@@ -1,12 +1,3 @@
-package model
-
-import (
-	"bytes"
-	"github.com/bitly/go-simplejson"
-	"fmt"
-
-)
-
 /**
  *
  * Create BY YooDing
@@ -17,30 +8,55 @@ import (
  *
  * <a href="https://github.com/YooDing/gone">Github</a>
  */
+package model
+
+import (
+	"github.com/bitly/go-simplejson"
+	"gone/utils"
+	"os"
+
+)
 //
 //type ZipUrl struct {
 //	ZipName string
 //	ZipPath string
 //}
-
 type ConfigData struct {
 	Version string
-	Jdk string
-	tomcat string
+	Jdk     string
+	tomcat  string
 }
 
-var(
-	res *simplejson.Json
+var (
+	res    *simplejson.Json
+	Server ConfigData
 )
 
-func init(){
+func init() {
 	str := `{
 		"version": "0.0.6",
-		"jdk1.8": "www.qq.com",
+		"jdk": "www.qq.com",
         "tomcat": "www.baidu.com"
 	}`
-	buf := bytes.NewBuffer([]byte(str))
-	js, _ := simplejson.NewFromReader(buf)
-	fmt.Println(js.Get("version").String())
+	//buf := bytes.NewBuffer([]byte(str))
+	//js, _ := simplejson.NewFromReader(buf)
+	//fmt.Println(js.Get("version").String())
 	res, _ = simplejson.NewJson([]byte(str))
+	version, _ := res.Get("version").String()
+	jdk, _ := res.Get("jdk").String()
+	tomcat, _ := res.Get("tomcat").String()
+	Server = ConfigData{version, jdk, tomcat}
+}
+
+func parsing(jsonStr string) ConfigData {
+	res, _ = simplejson.NewJson([]byte(jsonStr))
+	version, err := res.Get("version").String()
+	jdk, _ := res.Get("jdk").String()
+	tomcat, _ := res.Get("tomcat").String()
+	if err != nil {
+		utils.Error("程序解析远程仓库配置json文件失败！！！！请你稍后重试！")
+		//发生错误程序退出
+		os.Exit(1)
+	}
+	return ConfigData{version, jdk, tomcat}
 }
